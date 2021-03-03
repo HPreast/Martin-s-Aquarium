ce9431f344e7b7dd1d0444848ccbec46990154af
HPreast <preasths1@gmail.com>
2021-03-03 16:30:31 -0600

    Added js to automate locations in index.html

---
+++ scripts/locations/locationsData.js
@@ -0,0 +1,11 @@
+const locationCollection = [
+    "Pacific Ocean",
+    "Bali",
+    "Atlantic Ocean",
+    "Gulf of Mexico",
+    "Mississippi River",
+]
+
+export const getLocation = () => {
+    return locationCollection
+}
